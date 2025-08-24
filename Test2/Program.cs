ef721e7a8a8a634a6ad5cb33d397a2014fd95c3b
StephenVivash <stephenvivash@hotmail.com>
2025-08-24 18:53:22 +1000

    Implement AddOne

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello .NET!");
+
+Console.WriteLine("AddOne(1) = {0}", AddOne(1));
+Console.WriteLine("AddOne(2) = {0}", AddOne(2));
+Console.WriteLine("AddOne(3) = {0}", AddOne(3));
+
+int AddOne(int x) => x + 1;	
